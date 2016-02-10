c191c68bd6e072b52ac0bddc89f579b5a8a84ec3
Alexey Raga <alexey.raga@gmail.com>
2016-02-11 00:21:00 +1100

    tests

--- test/src/autocluster_consul_tests.erl
+++ test/src/autocluster_consul_tests.erl
@@ -30,7 +30,7 @@ reg_body_service_with_prefix_test() ->
 ttl_value_test() ->
   ?assertEqual(list_to_atom("30s"), autocluster_consul:ttl(30)).
 
-full_service_id_test() ->
+full_service_id_no_prefix_test() ->
   ?assertEqual("test", autocluster_consul:full_service_id("undefined", "test")).
 
 full_service_id_test() ->
