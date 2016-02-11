260f887d3a9b23743c2a7a7d675b2373d17db56a
John Ky <newhoggy@gmail.com>
2016-02-11 17:26:05 +1100

    Release.

--- deploy-release.sh
+++ deploy-release.sh
@@ -2,12 +2,17 @@
 
 ARTIFACT_PATH="$(find "$HOME/rabbitmq-public-umbrella/autocluster/dist" -maxdepth 1 -name '*.ez' | head -n 1)"
 ARTIFACT_NAME="$(basename "$ARTIFACT_PATH")"
-echo Releasing "$ARTIFACT_PATH"
+
+echo Installing github-release
 go get github.com/aktau/github-release
+
+echo Creating release
 github-release release \
   --user "$CIRCLE_PROJECT_USERNAME" \
   --repo "$CIRCLE_PROJECT_REPONAME" \
   --tag `git describe --tags` || echo "Release already created"
+
+echo Adding artifact tp release
 github-release upload \
   --user "$CIRCLE_PROJECT_USERNAME" \
   --repo "$CIRCLE_PROJECT_REPONAME" \
